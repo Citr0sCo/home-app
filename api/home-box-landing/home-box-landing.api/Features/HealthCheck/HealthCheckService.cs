bff439cc3a80c6ea54dff880ce3a146f3472207b
Milosz Dura <miloszdura@gmail.com>
2023-01-09 20:00:28 +0000

    handling exceptions thrown by ssl for some reason, to be investigated later

--- api/home-box-landing/home-box-landing.api/Features/HealthCheck/HealthCheckService.cs
+++ api/home-box-landing/home-box-landing.api/Features/HealthCheck/HealthCheckService.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using home_box_landing.api.Features.HealthCheck.Types;
 
 namespace home_box_landing.api.Features.HealthCheck
@@ -14,15 +15,26 @@ namespace home_box_landing.api.Features.HealthCheck
         public HealthCheckResponse PerformHealthCheck(string url, bool isSecure)
         {
             var prefix = isSecure ? "https" : "http";
-            
-            var result = _httpClient.GetAsync($"{prefix}://{url}").Result;
-            var responseMessage = result.Content.ReadAsStringAsync().Result;
-                       
-            return new HealthCheckResponse
+
+            try
             {
-                StatusCode = result.StatusCode,
-                StatusDescription = result.ReasonPhrase
-            };
+                var result = _httpClient.GetAsync($"{prefix}://{url}").Result;
+                var responseMessage = result.Content.ReadAsStringAsync().Result;
+
+                return new HealthCheckResponse
+                {
+                    StatusCode = result.StatusCode,
+                    StatusDescription = result.ReasonPhrase
+                };
+            }
+            catch (Exception)
+            {
+                return new HealthCheckResponse
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    StatusDescription = "Exception was thrown!"
+                };
+            }
         }
     }
 }
