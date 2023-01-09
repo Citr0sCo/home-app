5b6adb6638ebb5894d5a3e52011177d6ac5e33a3
Milosz Dura <miloszdura@gmail.com>
2023-01-09 14:05:34 +0000

    improving deploy script to automatically remove existing image/container and recretre

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,24 @@
     GREEN='\033[0;32m' # Green
     NC='\033[0m' # No Color
 
-    echo -e "${GREEN}[1/4] - Updating repo...${NC}";
+    echo -e "${GREEN}[1/7] - Stopping docker container...${NC}";
+    docker stop home-app
+
+    echo -e "${GREEN}[2/7] - Removing docker container...${NC}";
+    docker rm home-app
+
+    echo -e "${GREEN}[3/7] - Removing docker image...${NC}";
+    docker image rm home-box-image
+
+    echo -e "${GREEN}[4/7] - Updating repo...${NC}";
     git pull
 
-    echo -e "${GREEN}[2/4] - Compiling assets...${NC}";
+    echo -e "${GREEN}[5/7] - Compiling assets...${NC}";
     npm run ng b
 
-    echo -e "${GREEN}[3/4] - Creating fresh docker image...${NC}";
+    echo -e "${GREEN}[6/7] - Creating fresh docker image...${NC}";
     docker build -t home-box-image .
 
-    echo -e "${GREEN}[4/4] - Creating fresh docker container...${NC}";
+    echo -e "${GREEN}[7/7] - Creating fresh docker container...${NC}";
     docker compose up -d
 }
