1767868521e8d8bcaafc863615b65db54ed37bcc
Milosz Dura <miloszdura@gmail.com>
2023-02-21 15:26:27 +0000

    parsing dates correctly

--- src/services/deploy-service/deploy.mapper.ts
+++ src/services/deploy-service/deploy.mapper.ts
@@ -2,13 +2,13 @@ import { IDeploy } from './types/deploy.type';
 
 export class DeployMapper {
 
-    static map(response: any) : Array<IDeploy> {
+    static map(response: any): Array<IDeploy> {
         return response.map((deploy: any) => {
             return {
                 identifier: deploy.identifier,
                 commitId: deploy.commitId,
-                startedAt: deploy.startedAt,
-                finishedAt: deploy.finishedAt
+                startedAt: new Date(deploy.startedAt),
+                finishedAt: new Date(deploy.finishedAt)
             };
         });
     }
