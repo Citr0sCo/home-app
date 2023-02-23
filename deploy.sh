f42f17aba7f3c708f8bced7e8503fc289536874b
Milosz Dura <miloszdura@gmail.com>
2023-02-23 14:12:03 +0000

    adding test to properly test a service, correcting deploy script to pull changes first before attempting to reset

--- deploy.sh
+++ deploy.sh
@@ -4,29 +4,32 @@
   GREEN='\033[0;32m' # Green
   NC='\033[0m'       # No Color
 
-  echo -e "${GREEN}[1/8] - Stopping docker containers...${NC}"
+  echo -e "${GREEN}[1/9] - Stopping docker containers...${NC}"
   docker stop home-app
   docker stop home-app-db
 
-  echo -e "${GREEN}[2/8] - Removing docker containers...${NC}"
+  echo -e "${GREEN}[2/9] - Removing docker containers...${NC}"
   docker rm home-app
   docker rm home-app-db
 
-  echo -e "${GREEN}[3/8] - Removing docker image...${NC}"
+  echo -e "${GREEN}[3/9] - Removing docker image...${NC}"
   docker image rm home-box-image
 
-  echo -e "${GREEN}[4/8] - Updating repo...${NC}"
+  echo -e "${GREEN}[4/9] - Updating repo...${NC}"
+  git pull
+
+  echo -e "${GREEN}[4/9] - Switching revision...${NC}"
   git reset --hard $LATEST
 
-  echo -e "${GREEN}[5/8] - Pulling packages...${NC}"
+  echo -e "${GREEN}[5/9] - Pulling packages...${NC}"
   yarn
 
-  echo -e "${GREEN}[6/8] - Compiling assets...${NC}"
+  echo -e "${GREEN}[6/9] - Compiling assets...${NC}"
   npm run ng b
 
-  echo -e "${GREEN}[7/8] - Creating fresh docker image...${NC}"
+  echo -e "${GREEN}[7/9] - Creating fresh docker image...${NC}"
   docker build -t home-box-image .
 
-  echo -e "${GREEN}[8/8] - Creating fresh docker container...${NC}"
+  echo -e "${GREEN}[8/9] - Creating fresh docker container...${NC}"
   docker compose up -d
 }
