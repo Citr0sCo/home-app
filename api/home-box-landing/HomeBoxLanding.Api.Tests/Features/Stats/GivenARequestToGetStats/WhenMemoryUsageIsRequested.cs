3911aec55de787c2e6aeeec7e143f693542e1968
Milosz Dura <miloszdura@gmail.com>
2023-03-05 13:12:18 +0000

    fixing tests

--- api/home-box-landing/HomeBoxLanding.Api.Tests/Features/Stats/GivenARequestToGetStats/WhenMemoryUsageIsRequested.cs
+++ api/home-box-landing/HomeBoxLanding.Api.Tests/Features/Stats/GivenARequestToGetStats/WhenMemoryUsageIsRequested.cs
@@ -15,7 +15,7 @@ namespace HomeBoxLanding.Api.Tests.Features.Stats.GivenARequestToGetStats
         public void SetUp()
         {
             var shellService = new Mock<IShellService>();
-            shellService.Setup(x => x.RunOnHost("docker stats home-app --no-stream")).Returns("CONTAINER ID   NAME       CPU %     MEM USAGE / LIMIT     MEM %     NET I/O         BLOCK I/O     PIDS\ne5b9df6f7579   home-app   0.01%     89.39MiB / 7.764GiB   1.12%     956kB / 1.4MB   1.11MB / 0B   22");
+            shellService.Setup(x => x.RunOnHost("docker stats --no-stream")).Returns("CONTAINER ID   NAME       CPU %     MEM USAGE / LIMIT     MEM %     NET I/O         BLOCK I/O     PIDS\ne5b9df6f7579   home-app   0.01%     89.39MiB / 7.764GiB   1.12%     956kB / 1.4MB   1.11MB / 0B   22");
             
             var subject = new StatsService(shellService.Object, new FakeStatsServiceCache());
             _result = subject.GetServerStats();
