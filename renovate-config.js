ac87f7821a534d91e765a316a4faf4ad141ee42b
Milosz Dura <miloszdura@gmail.com>
2025-03-27 15:59:24 +0000

    adding renovate workflow

---
+++ renovate-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    branchPrefix: 'renovate/',
+    username: 'renovate-release',
+    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+    onboarding: true,
+    platform: 'github',
+    forkProcessing: 'enabled',
+    dryRun: 'full',
+    repositories: ['citr0s/home-app'],
+    packageRules: [
+        {
+            description: 'lockFileMaintenance',
+            matchUpdateTypes: [
+                'pin',
+                'digest',
+                'patch',
+                'minor',
+                'major',
+                'lockFileMaintenance',
+            ],
+            dependencyDashboardApproval: true,
+            minimumReleaseAge: '0 days',
+        },
+    ],
+};
