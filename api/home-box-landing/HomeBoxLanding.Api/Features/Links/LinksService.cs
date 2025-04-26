73924d040a3405bc52734a0c27e55199d4da7f3b
Milosz Dura <miloszdura@gmail.com>
2025-04-26 09:54:05 +0100

    debugging link file upload not working (#228)

--- api/home-box-landing/HomeBoxLanding.Api/Features/Links/LinksService.cs
+++ api/home-box-landing/HomeBoxLanding.Api/Features/Links/LinksService.cs
@@ -202,22 +202,3 @@ public class LinksService
         return response;
     }
 }
-
-public class LinkMapper
-{
-    public static Link Map(LinkRecord record)
-    {
-        return new Link
-        {
-            Identifier = record.Identifier,
-            Name = record.Name,
-            IconUrl = record.IconUrl,
-            IsSecure = record.IsSecure,
-            Port = record.Port,
-            Host = record.Host,
-            Url = record.Url,
-            Category = record.Category,
-            SortOrder = record.SortOrder
-        };
-    }
-}
